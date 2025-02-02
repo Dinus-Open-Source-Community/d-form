e400d3cbf9c47d4569cd9f2e695f27392b6c5c14
naufalzp <naufalarya0@gmail.com>
2025-02-02 21:39:56 +0700

    chore: initialize React app with Vite and Tailwind CSS setup

---
+++ d-form/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="text-center flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">
+        D-Form lebih keren dari G-Form
+      </h1>
+    </div>
+  );
+};
+
+export default App;
