fc60e8bfe25336f6804f06146f765c91a0eaa147
naufalzp <naufalarya0@gmail.com>
2025-02-02 21:24:00 +0700

    remove unused files and configurations from API and frontend

--- api/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { helloHandler } = require('../controllers/helloController');
-
-const router = express.Router();
-
-router.get('/hello', helloHandler);
-
-module.exports = router;
