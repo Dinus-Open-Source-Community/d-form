289ca34a224aca3b01a69a0fc57b350517925012
Fachruddin <poenyoex@gmail.com>
2025-03-04 12:24:46 +0700

    fix: adjust gap in Home component heading for improved layout

--- d-form/src/pages/Home.jsx
+++ d-form/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     <>
       <div className="mt-16 sm:mt-22 bg-[#343434] text-white text-center py-16 sm:py-24 md:py-32 lg:py-40 px-4">
         <div className="flex flex-col items-center justify-center text-center mb-6 sm:mb-8">
-          <div className="flex items-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-widest uppercase gap-3 sm:gap-4 md:gap-5">
+          <div className="flex flex-wrap justify-center items-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-widest uppercase gap-2 sm:gap-4 md:gap-5">
             <span>WELCOME TO</span>
             <span className="text-6xl sm:text-8xl md:text-9xl lg:text-[100px] font-extrabold leading-none">
               D
