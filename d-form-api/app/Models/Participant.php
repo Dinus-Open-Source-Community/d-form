06dc148d1dc1817f534178fb1417321d2a84e6ef
naufalzp <naufalarya0@gmail.com>
2025-04-15 10:53:16 +0700

    feat: update Participant model and migration to use UUIDs for participant IDs and foreign UUIDs for event IDs

--- d-form-api/app/Models/Participant.php
+++ d-form-api/app/Models/Participant.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+    use HasUuids, HasFactory;
+    
     protected $fillable = [
         'event_id',
         'name',
-        'token',
         'school',
         'is_presence',
         'presence_at',
