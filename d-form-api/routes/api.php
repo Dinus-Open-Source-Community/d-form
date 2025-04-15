00889d4e0c3d49983b4026341a385ddfe4df4b27
naufalzp <naufalarya0@gmail.com>
2025-04-15 10:53:48 +0700

    feat: add event routes for managing events in the API

--- d-form-api/routes/api.php
+++ d-form-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,5 +12,13 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('user', [AuthController::class, 'user']);
         Route::post('logout', [AuthController::class, 'logout']);
+
+        Route::prefix('events')->group(function () {
+            Route::get('/', [EventController::class, 'index']);
+            Route::post('/', [EventController::class, 'store']);
+            Route::get('{id}', [EventController::class, 'show']);
+            Route::put('{id}', [EventController::class, 'update']);
+            Route::delete('{id}', [EventController::class, 'destroy']);
+        });
     });
 });
