d9ada509ce7ad9b5980a12ed8afe8bcb2458bc84
naufalzp <naufalarya0@gmail.com>
2025-04-16 09:27:39 +0700

    feat: add routes for participant uploads and barcode downloads

--- d-form-api/routes/api.php
+++ d-form-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\ParticipantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,11 @@ Route::prefix('v1')->group(function () {
             Route::get('{id}', [EventController::class, 'show']);
             Route::put('{id}', [EventController::class, 'update']);
             Route::delete('{id}', [EventController::class, 'destroy']);
+        });
+
+        Route::prefix('participants')->group(function () {
+            Route::post('upload/{eventId}', [ParticipantController::class, 'uploadParticipants']);
+            Route::get('download/{eventId}/{participantId?}', [ParticipantController::class, 'handleBarcodeDownload']);
         });
     });
 });
