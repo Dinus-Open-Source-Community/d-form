8897b9baa0466f05d453bd6f880348de8c215ec7
naufalzp <naufalarya0@gmail.com>
2025-05-15 00:34:54 +0700

    fix: Update user seeding to create Super Admin with specific credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super Admin',
+            'email' => 'admin@mail.com',
+        ]);
+
+        $this->call([
+            EventSeeder::class,
         ]);
     }
 }
