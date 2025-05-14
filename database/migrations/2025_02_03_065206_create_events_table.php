81f9f38c4f5b52e377194b0ea80a0325df0e71c8
naufalzp <naufalarya0@gmail.com>
2025-05-15 00:34:25 +0700

    feat: Create migration for events table with necessary fields and relationships

---
+++ database/migrations/2025_02_03_065206_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')->constrained()->onDelete('restrict');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 15, 2)->default(0);
+            $table->string('cover_event')->nullable();
+            $table->text('address');
+            $table->string('map_url')->nullable();
+            $table->string('gform_url')->nullable();
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->integer('duration_days');
+            $table->integer('participants');
+            $table->enum('type', ['rkt', 'non-rkt']);
+            $table->enum('division', ['general', 'programming', 'multimedia', 'networking'])->default('general');
+
+            $table->date("start_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
