061664a6957366f8ba33b93b01ed109722e2826f
naufalzp <naufalarya0@gmail.com>
2025-05-15 19:50:06 +0700

    feat: add PageHeader component for admin layout

---
+++ app/Livewire/Admin/Layouts/PageHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Layouts;
+
+use Livewire\Component;
+
+class PageHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.layouts.page-header');
+    }
+}
