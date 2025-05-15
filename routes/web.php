7268d202af2fbdf6466c7f0b355aacb1fd9cb491
naufalzp <naufalarya0@gmail.com>
2025-05-15 19:50:17 +0700

    feat: add admin routes for dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Livewire\Admin\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Admin routes
+Route::group([
+    'prefix' => 'admin',
+    // 'middleware' => 'auth',
+    'as' => 'admin.'
+], function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
