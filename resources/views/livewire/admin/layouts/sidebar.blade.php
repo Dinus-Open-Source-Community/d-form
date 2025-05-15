71d0753955a0db4583c6d2b520c9a1fb4ccd5d95
naufalzp <naufalarya0@gmail.com>
2025-05-15 19:49:47 +0700

    feat: add sidebar layout for admin panel

---
+++ resources/views/livewire/admin/layouts/sidebar.blade.php
@@ -0,0 +1,51 @@
+<div class="min-h-screen h-full w-80 bg-[#343434] text-white flex flex-col z-40 flex-shrink-0">
+  {{-- Logo --}}
+  <div class="p-4 flex justify-center items-center">
+    <h1 class="text-5xl font-bold">D</h1>
+    <span class="ml-2 text-4xl">FORM</span>
+  </div>
+
+  {{-- Tombol New Event --}}
+  <div class="p-6 flex justify-center">
+    <a href="">
+      <button class="bg-white/10 text-white rounded-lg py-2 px-4 text-center hover:bg-white/20 cursor-pointer">
+        New Event
+      </button>
+    </a>
+  </div>
+
+  {{-- Menu Items --}}
+  <div class="flex-grow overflow-y-auto">
+    <a href="{{ route('admin.dashboard') }}"wire:navigate>
+      <div
+        class="flex items-center rounded-lg p-4 m-2 {{ request()->is('admin/dashboard') ? 'bg-white/10' : '' }} hover:bg-white/10 cursor-pointer">
+        <span class="material-icons mr-2">menu</span>
+        Dashboard
+      </div>
+    </a>
+    <a href="" wire:navigate>
+      <div
+        class="flex items-center rounded-lg p-4 m-2 {{ request()->is('admin/events') ? 'bg-white/10' : '' }}  hover:bg-white/10 cursor-pointer">
+        <span class="material-icons mr-2">menu</span>
+        Events
+      </div>
+    </a>
+  </div>
+
+  {{-- User Info --}}
+  <div class="mt-auto p-4">
+    <div class="p-3 m-3 flex items-center rounded-md bg-white/10">
+      {{-- Avatar --}}
+      <div class="w-10 h-10 bg-gray-200 rounded-md mr-3 flex-shrink-0"></div>
+
+      {{-- Username & Email --}}
+      <div class="flex flex-col flex-grow w-0">
+        <p class="font-medium">{{ Auth::user()->name ?? 'Admin' }}</p>
+        <p class="text-xs text-gray-300 break-all w-full">{{ Auth::user()->email ?? 'example@mail.com' }}</p>
+      </div>
+
+      {{-- Menu Button --}}
+      <button class="mx-auto">⋮</button>
+    </div>
+  </div>
+</div>
