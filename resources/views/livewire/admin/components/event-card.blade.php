b168a1bdce8a2fdaa678a6898989dc1a3a016667
naufalzp <naufalarya0@gmail.com>
2025-05-17 01:01:21 +0700

    fix: Correctly display cover event image in event card component

--- resources/views/livewire/admin/components/event-card.blade.php
+++ resources/views/livewire/admin/components/event-card.blade.php
@@ -3,7 +3,11 @@
     href="{{ route('admin.event-detail', $event['id']) }}"
     wire:navigate
 >
-    <div class="h-36 sm:h-40 md:h-48 bg-[#343434]"></div>
+    @if (!empty($event['cover_event']))
+        <img src="{{ $event['cover_event'] }}" alt="{{ $event['name'] }} Cover" class="h-36 sm:h-40 md:h-48 w-full object-cover" />
+    @else
+        <div class="h-36 sm:h-40 md:h-48 bg-[#343434]"></div>
+    @endif
 
     <div class="p-3 sm:p-4 border-x-2 border-b-2 rounded-b-lg border-gray-400 flex flex-col flex-grow">
         <div class="flex flex-wrap gap-2 mb-2">
