cb102db57a2fd9488468247f9add6a206d86ba1c
naufalzp <naufalarya0@gmail.com>
2025-05-16 17:03:50 +0700

    fix: Adjust layout styles for admin panel to improve responsiveness

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -1,9 +1,9 @@
 <x-layouts.app>
-  <div class="flex min-h-screen w-full bg-gray-100">
+  <div class="flex h-screen w-full bg-gray-100">
     <livewire:admin.layouts.sidebar />
     <div class="flex flex-col flex-grow h-full">
       <livewire:admin.layouts.page-header />
-      <div class="flex-grow mt-[1rem] px-6 py-4 overflow-auto">
+      <div class="flex-grow px-6 py-4 overflow-auto">
         {{ $slot }}
       </div>
     </div>
