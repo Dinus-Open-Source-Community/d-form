994e447e4c938527d3fc3270c7c0fd0acda00ba3
Fachruddin <poenyoex@gmail.com>
2025-06-07 13:41:34 +0700

    feat: Add title attribute and improve filter functionality in Events component

--- app/Livewire/Client/Events.php
+++ app/Livewire/Client/Events.php
@@ -4,12 +4,29 @@ namespace App\Livewire\Client;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 
 class Events extends Component
 {
-
     #[Layout('components.layouts.client', ['title' => 'Events'])]
+    #[Title('Events - D-Form')]
     
+    public $search = '';
+    public $categoryFilter = '';
+
+    public function mount()
+    {
+        $this->search = request()->get('search', '');
+        $this->categoryFilter = request()->get('category', '');
+    }
+
+    public function clearFilters()
+    {
+        $this->search = '';
+        $this->categoryFilter = '';
+        // Jangan lakukan redirect di sini, biarkan Livewire yang update state
+    }
+
     public function render()
     {
         return view('livewire.client.events');
