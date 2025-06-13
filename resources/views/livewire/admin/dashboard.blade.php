3477f9bb7bf4ee4011c27f344557ba87821558d3
naufalzp <naufalarya0@gmail.com>
2025-06-13 13:49:48 +0700

    fix: update QR code usage in event list component for dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 <div class="p-4">
   <h1 class="text-3xl font-bold mb-8">Dashboard</h1>
   <livewire:admin.components.dashboard-stats />
-  <livewire:admin.components.event-list :showToday="true" :showOngoing="false" :showUpcoming="false" :useQR="true" />
+  <livewire:admin.components.event-list :showToday="true" :showOngoing="false" :showUpcoming="false" :useQR="false" />
 </div>
